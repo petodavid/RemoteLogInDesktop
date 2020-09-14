a82f611210a238f7660a64b18c287b48b1349fe3
David <david.peto1999@gmail.com>
2020-09-14 15:50:45 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace remoteLogIn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uidText = new System.Windows.Forms.Label();
+            this.qrCodePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.qrCodePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uidText
+            // 
+            this.uidText.AutoSize = true;
+            this.uidText.Location = new System.Drawing.Point(12, 982);
+            this.uidText.Name = "uidText";
+            this.uidText.Size = new System.Drawing.Size(70, 25);
+            this.uidText.TabIndex = 0;
+            this.uidText.Text = "label1";
+            // 
+            // qrCodePictureBox
+            // 
+            this.qrCodePictureBox.Location = new System.Drawing.Point(-3, -4);
+            this.qrCodePictureBox.Name = "qrCodePictureBox";
+            this.qrCodePictureBox.Size = new System.Drawing.Size(1012, 983);
+            this.qrCodePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.qrCodePictureBox.TabIndex = 1;
+            this.qrCodePictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 1016);
+            this.Controls.Add(this.qrCodePictureBox);
+            this.Controls.Add(this.uidText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Remote LogIn";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.qrCodePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label uidText;
+        private System.Windows.Forms.PictureBox qrCodePictureBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using Google.Cloud.Firestore;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace remoteLogIn
+{
+    public partial class Form1 : Form
+    {
+        string projectId = "remotelogin-33dbe";
+        string uid;
+        FirestoreDb db;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/config.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filepath);
+            db = FirestoreDb.Create(projectId);
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("remoteLogIn");
+
+            if (key.GetValue("uid") != null)
+            {
+                uid = key.GetValue("uid").ToString();
+            }
+            else
+            {
+                uid = Guid.NewGuid().ToString();
+                key.SetValue("uid", uid);
+            }
+            key.Close();
+            uidText.Text = uid;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(uid, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            qrCodePictureBox.Image = qrCode.GetGraphic(80);
+            checkForUpdates();
+        }
+
+        private async void checkForUpdates()
+        {
+            DocumentReference docRef = db.Collection("client").Document(uid);
+            Dictionary<string, object> data = new Dictionary<string, object> { };
+            await docRef.SetAsync(data);
+            FirestoreChangeListener listener = docRef.Listen(async snapshot =>
+            {
+                if (snapshot.Exists)
+                {
+                    Dictionary<string, Object> snap = snapshot.ToDictionary();
+                    if (!snap.ContainsKey("page") || !snap.ContainsKey("userName") || !snap.ContainsKey("password"))
+                    {
+                        return;
+                    }
+                    String page = snap["page"].ToString();
+                    String userName = snap["userName"].ToString();
+                    String password = snap["password"].ToString();
+                    openPage(page, password, userName);
+                    await docRef.SetAsync(data);
+                }
+            });
+        }
+
+        private async void openPage(String page, String password, String username)
+        {
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+            DocumentReference docRef = db.Collection("page").Document(page);
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+            if (snapshot.Exists)
+            {
+                Dictionary<string, Object> snap = snapshot.ToDictionary();
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments("start-maximized");
+                try
+                {
+                    var driver = new ChromeDriver(driverService, options);
+                    driver.Navigate().GoToUrl(snap["domain"].ToString());
+                    driver.FindElement(By.Name(snap["userNameFieldID"].ToString())).SendKeys(username);
+                    driver.FindElement(By.Name(snap["passwordFieldID"].ToString())).SendKeys(password);
+                    driver.FindElement(By.Name(snap["submitButtonID"].ToString())).Click();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+
+        }
+
+    }
+}
